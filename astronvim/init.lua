43716abfb30d753745efc10c467775c779b4015c
Mario <max_cable@hotmail.com>
2023-12-17 12:52:48 -0600

    move options

--- astronvim/init.lua
+++ astronvim/init.lua
@@ -1,19 +1,10 @@
 return {
-  colorscheme = "catppuccin",
-
   plugins = {
     "AstroNvim/astrocommunity",
     { import = "astrocommunity.colorscheme.rose-pine" },
     { import = "astrocommunity.colorscheme.tokyonight-nvim" },
-    { import = "astrocommunity.colorscheme.kanagawa-nvim" },
     { import = "astrocommunity.colorscheme.monokai-pro-nvim" },
     { import = "astrocommunity.colorscheme.catppuccin" },
-    { import = "astrocommunity.colorscheme.dracula-nvim" },
-    -- { import = "astrocommunity.colorscheme.nightfox-nvim" },
-    -- { import = "astrocommunity.colorscheme.onedarkpro-nvim" },
-    -- { import = "astrocommunity.colorscheme.poimandres-nvim" },
-    -- { import = "astrocommunity.bars-and-lines.bufferline-nvim" },
-    -- { import = "astrocommunity.bars-and-lines.lualine-nvim" },
     { import = "astrocommunity.editing-support.todo-comments-nvim" },
     { import = "astrocommunity.motion.hop-nvim" },
     { import = "astrocommunity.motion.vim-matchup" },
@@ -21,29 +12,29 @@ return {
     { import = "astrocommunity.pack.bash" },
     { import = "astrocommunity.pack.json" },
     { import = "astrocommunity.pack.markdown" },
-    -- { import = "astrocommunity.pack.php" },
+    { import = "astrocommunity.pack.php" },
     { import = "astrocommunity.pack.svelte" },
     { import = "astrocommunity.pack.tailwindcss" },
     { import = "astrocommunity.pack.toml" },
-    -- { import = "astrocommunity.pack.typescript" },
+    { import = "astrocommunity.pack.typescript" },
     { import = "astrocommunity.pack.vue" },
     { import = "astrocommunity.pack.yaml" },
     { import = "astrocommunity.scrolling.neoscroll-nvim" },
-    { "rose-pine", lazy = false },
-    { "tokyonight.nvim", lazy = false },
-    { "monokai-pro.nvim", lazy = false },
-    { "kanagawa.nvim", lazy = false },
-    { "lunarvim/lunar.nvim", lazy = false },
-    { "wavded/vim-stylus", lazy = false },
-    { "mattn/emmet-vim", lazy = false, init = function() vim.g.user_emmet_leader_key = "<C-L>" end },
+    { "rose-pine",                                                 lazy = false },
+    { "catppuccin",                                                 lazy = false },
+    { "tokyonight.nvim",                                           lazy = false },
+    { "monokai-pro.nvim",                                          lazy = false },
+    { "wavded/vim-stylus",                                         lazy = false },
+    {
+      "mattn/emmet-vim",
+      lazy = false,
+      init = function()
+        vim.g.user_emmet_leader_key =
+        "<C-L>"
+      end
+    },
     { "tpope/vim-surround", lazy = false },
-    { "tpope/vim-repeat", lazy = false },
-    { "windwp/nvim-ts-autotag", lazy = false, setup = {
-      autotag = {
-        enable = true,
-      },
-    } },
-    { "hhsnopek/vim-sugarss", lazy = false },
+    { "tpope/vim-repeat",   lazy = false },
     {
       "nvim-telescope/telescope.nvim",
       opts = {
@@ -58,26 +49,21 @@ return {
         },
       },
     },
-  },
-  opt = {
-    relativenumber = false,
-  },
-  lsp = {
-    formatting = {
-      format_on_save = true,
-    },
-    servers = {
-      "stylelint_lsp",
-    },
-    config = {
-      stylelint_lsp = {
-        cssInJs = false,
-        filetypes = { "css", "scss", "sass", "postcss", "sugarss", "styl", "stylus", "sss" },
-        validateOnType = true,
-        validateOnSave = true,
-        autoFixOnFormat = true,
-        autoFixOnSave = true,
-      },
-    },
+    {
+      "NvChad/nvim-colorizer.lua",
+      opts = {
+        filetypes = { "*" },
+        user_default_options = {
+          names = true,
+          RRGGBBAA = true,
+          rgb_fn = true,
+          hsl_fn = true,
+          css = true,
+          css_fn = true,
+          tailwind = true,
+          sass = { enable = true, parsers = { "css" }, },
+        }
+      }
+    }
   },
 }
--- astronvim/options.lua
+++ astronvim/options.lua
@@ -5,6 +5,19 @@ return {
   lsp = {
     formatting = {
       format_on_save = true,
-    }
+    },
+    servers = {
+      "stylelint_lsp",
+    },
+    config = {
+      stylelint_lsp = {
+        cssInJs = false,
+        filetypes = { "css", "scss", "sass", "postcss", "sugarss", "styl", "stylus", "sss" },
+        validateOnType = true,
+        validateOnSave = true,
+        autoFixOnFormat = true,
+        autoFixOnSave = true,
+      },
+    },
   },
 }
